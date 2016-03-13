cc8dbe5f9dd4460cc577c7466258bf157a426558
Jason Karns <jason@karns.name>
2016-03-13 17:48:26 -0400

    number and nil specs for Utility.replacer

--- spec/lib/bumper/utility_spec.rb
+++ spec/lib/bumper/utility_spec.rb
@@ -23,6 +23,26 @@ module Bumper
           Then { replace("FOO = '1.2.3'" , "FOO", "'4.5.6'") == "FOO = '4.5.6'" }
         end
       end
+
+      context "number" do
+        context "without spaces" do
+          Then { replace("FOO='1.2.3'", "FOO", 42) == "FOO=42" }
+        end
+
+        context "with spaces" do
+          Then { replace("FOO =  '1.2.3'" , "FOO", "42") == "FOO =  42" }
+        end
+      end
+
+      context "nil" do
+        context "without spaces" do
+          Then { replace("FOO='1.2.3'", "FOO", "nil") == "FOO=nil" }
+        end
+
+        context "with spaces" do
+          Then { replace("FOO =  '1.2.3'" , "FOO", "nil") == "FOO =  nil" }
+        end
+      end
     end
   end
 end
