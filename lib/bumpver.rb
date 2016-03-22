0dc8673880147abb37be3dcb5554398863fad7bf
Kyle Tolle <kyle@nullsix.com>
2016-03-21 20:27:21 -0600

    Require classes in Bumpver
    Setting the load path makes the requires in this and other Bumpver
    files look in bumpver’s lib folder first.
    
    Requiring the version and version_file files here allows us to access
    them easily from another ruby program or IRB. Bumpver is the entry
    point for all its other functionality.

--- lib/bumpver.rb
+++ lib/bumpver.rb
@@ -1,2 +1,8 @@
+lib = File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 module Bumpver
 end
+
+require 'bumpver/version'
+require 'bumpver/version_file'
